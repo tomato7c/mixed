039992ea48d6e3ac5f02e71d95fa895d9d11ed5f
xuhongxu <xuhongxu@kuaishou.com>
2024-06-01 16:05:22 +0800

    init

---
+++ src/main/java/com/example/mixed/MixedApplication.java
@@ -0,0 +1,13 @@
+package com.example.mixed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mixed/MixedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mixed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
